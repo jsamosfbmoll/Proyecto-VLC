a34b9bb322913566c763a6cc16a0abc8746f42cd
sebastia98 <sadrover@cifpfbmoll.eu>
2019-11-21 18:25:17 +0100

    fix: Arreglar método verificar VLC
    Transformar el String que se guarda en la variable vlc en un texto crudo

--- VLC_random_playlist.py
+++ VLC_random_playlist.py
@@ -1,16 +1,21 @@
 from aleatoriedad_canciones import reconstruirLista
 import os
 def verificarVLC():
-    if os.access("C:\Program Files (x86)\VideoLAN\VLC\vlc.exe", os.F_OK):
-        vlc = "C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
-    elif os.access("C:\Program Files\VideoLAN\VLC\vlc.exe", os.F_OK):
-        vlc = "C:\Program Files\VideoLAN\VLC\vlc.exe"
+    if os.access(r"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe", os.F_OK):
+        vlc = r"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
+    elif os.access(r"C:\Program Files\VideoLAN\VLC\vlc.exe", os.F_OK):
+        vlc = r"C:\Program Files\VideoLAN\VLC\vlc.exe"
+    else:
+        print("No tienes VLC")
+    return vlc
 
 def reproducirCancion():
     
     listaCanciones = reconstruirLista()
     rutaVLC = verificarVLC()
-
+    os.popen(rutaVLC)
     
         
-    
+if __name__ == "__main__":
+    assert verificarVLC() == r"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
+    #reproducirCancion()
