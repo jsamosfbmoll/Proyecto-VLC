b84eee3c161aec9ff3082368c812f3872586c5c3
jsamosfbmoll <jsamos@cifpfbmoll.eu>
2019-11-21 19:39:44 +0100

    test: Añadir asserts
    Añadir asserts que comprueban la integridad de los datos al salir de la
    función getRutaCanción
    y añadir bloque try except en la función leerXML que comprueba que el
    documento XML libreria_canciones.xml
    este bien formado

--- lectura_xml.py
+++ lectura_xml.py
@@ -1,11 +1,18 @@
 import xml.etree.ElementTree as ET
 
 def leerXML():
-    arbol = ET.parse("libreria_canciones.xml")
+
+    try:
+        arbol = ET.parse("libreria_canciones.xml")
+    except ET.ParseError:
+        print("El XML no esta bien formado")
+        quit()
+
     raiz = arbol.getroot()
     return raiz, arbol
 
 def getNombresCanciones():
+
     raiz, arbol = leerXML()
     tracks = raiz[0][0][3]
     nombresCanciones = []
@@ -16,21 +23,32 @@ def getNombresCanciones():
     for track in tracks:
         nombreCancion = track.find("nombre").text
         nombresCanciones.append(nombreCancion)
+
     assert isinstance(nombresCanciones, list) and True if len(nombresCanciones) > 0 else False
+
     return nombresCanciones
 
 def getRutaCancion(cancion):
+
+    assert isinstance(cancion, str)
+    assert cancion != ""
+
     raiz, arbol = leerXML()
     albums = raiz.find("albums")
+    rutaCancion = ""
 
     for album in albums:
         tracks = album.find("tracks")
         for track in tracks:
             if track.find("nombre").text == cancion:
                 rutaCancion = track.find("ruta").text
+
+    assert isinstance(rutaCancion, str)
+    assert rutaCancion != ""
+
     return rutaCancion
 
 
 if __name__ == "__main__":
     assert getNombresCanciones() == ["Welcome to the jungle"]
-    assert getRutaCancion("Welcome to the jungle") == "desktop"
+    assert getRutaCancion("Welcome to the jungle") == r"C:\Users\Dual\Desktop"
