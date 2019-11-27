2fff615a8fdfb7fa1c776774803bd025c340cf3a
jsamosfbmoll <jsamos@cifpfbmoll.eu>
2019-11-27 20:22:50 +0100

    style: Añadir espacios
    Añadir espacios para formatear el código

--- VLC_random_playlist.py
+++ VLC_random_playlist.py
@@ -3,6 +3,7 @@ import os
 
 
 def verificarVLC():
+
     if os.access(r"C:\Program Files (x86)\VideoLAN\VLC\vlc.exe", os.F_OK):
         vlc = "\"C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe\""
     elif os.access(r"C:\Program Files\VideoLAN\VLC\vlc.exe", os.F_OK):
@@ -10,21 +11,28 @@ def verificarVLC():
     else:
         print("No tienes VLC")
         quit()
+
     return vlc
 
 
 def mostrarCanciones(canciones):
+
     print("Las canciones se reproducirán en este orden: ")
+
     for cancion in canciones:
         print("  " + cancion)
+
     return None
 
 
 def reproducirCanciones():
+
     rutasCanciones, nombresCanciones = asignarRutas()
     rutaVLC = verificarVLC()
+
     comando = rutaVLC + " " + rutasCanciones + " --no-repeat"
     mostrarCanciones(nombresCanciones)
+    
     os.popen(comando)
     return None
 
--- aleatoriedad_canciones.py
+++ aleatoriedad_canciones.py
@@ -2,23 +2,31 @@ from lectura_xml import getNombresCanciones, getRutaCancion
 import random
 
 
-def reconstruirLista():    
+def reconstruirLista():
+
     nombresCanciones = getNombresCanciones()
     copiaCanciones = nombresCanciones[:]
+
     assert isinstance(nombresCanciones, list)
+
     cancionesReordenadas = []
     for indice in range(0, len(nombresCanciones)):
         numeroAleatorio = random.randrange(len(nombresCanciones))
         cancionesReordenadas.append(nombresCanciones[numeroAleatorio])
         nombresCanciones.remove(cancionesReordenadas[indice])
+
     assert len(copiaCanciones) == len(cancionesReordenadas)
+
     return cancionesReordenadas
 
 
 def asignarRutas():
+
     canciones = reconstruirLista()
     listaRutasCanciones = []
+
     for cancion in canciones:
         listaRutasCanciones.append("\"" + getRutaCancion(cancion) + "\"")
+        
     rutasCanciones = " ".join(listaRutasCanciones)
     return rutasCanciones, canciones
