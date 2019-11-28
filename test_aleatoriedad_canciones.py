d2a630be69ef3d1dba81ffd36988c971b9c4bf42
jsamosfbmoll <jsamos@cifpfbmoll.eu>
2019-11-28 17:59:27 +0100

    test: Añadir casos test para aleatoriedad_canciones.py
    Añadir casos test para el módulo de la capa lógica, estos casos evaluan
    si la lista aleatoria devuelve una lista sin elementos repetidos y si la
    función asignarRutas devuelve un string y una lista

--- test_aleatoriedad_canciones.py
+++ test_aleatoriedad_canciones.py
@@ -1,4 +1,21 @@
 from aleatoriedad_canciones import *
 
-def test_1():
-    pass
+
+def test_aleatoria_cantidad():
+    assert len(reconstruirLista()) >= 8
+
+
+def test_aleatoria_no_repetidos():
+    canciones = reconstruirLista()
+    for cancion in canciones:
+        if canciones.count(cancion) > 1:
+            assert False
+    assert True
+
+
+def test_asignar_rutas_primer_valor():
+    assert isinstance(asignarRutas()[0], str)
+
+
+def test_asignar_rutas_segundo_valor():
+    assert isinstance(asignarRutas()[1], list)
